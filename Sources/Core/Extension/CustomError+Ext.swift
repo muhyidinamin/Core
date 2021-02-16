4bec2ec8fe8662eb37e275c42a3032ae048df392
Muhamad Muhyidin Amin <muhyidinxiitkj3@gmail.com>
2021-02-16 22:15:02 +0700

    update some code

--- Sources/Core/Extension/CustomError+Ext.swift
+++ Sources/Core/Extension/CustomError+Ext.swift
@@ -6,32 +6,31 @@
 //
 
 import Foundation
- 
+
 public enum URLError: LocalizedError {
- 
-  case invalidRequest
-  case invalidResponse
-  case addressUnreachable(URL)
-  
-  public var errorDescription: String? {
-      switch self {
-      case .invalidRequest: return "Request is null."
-      case .invalidResponse: return "The server responded with garbage."
-      case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
-      }
-  }
- 
-}
- 
-public enum DatabaseError: LocalizedError {
- 
-  case invalidInstance
-  case requestFailed
-  
+    case invalidRequest
+    case invalidResponse
+    case addressUnreachable(URL)
+    
     public var errorDescription: String? {
-    switch self {
-    case .invalidInstance: return "Database can't instance."
-    case .requestFailed: return "Your request failed."
+        switch self {
+        case .invalidRequest: return "Request is null."
+        case .invalidResponse: return "The server responded with garbage."
+        case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
+        }
     }
-  }
+    
+}
+
+public enum DatabaseError: LocalizedError {
+    
+    case invalidInstance
+    case requestFailed
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidInstance: return "Database can't instance."
+        case .requestFailed: return "Your request failed."
+        }
+    }
 }
